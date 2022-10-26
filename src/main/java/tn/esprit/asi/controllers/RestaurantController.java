f4bf4c62a0c830c78d34dc954818da237b77c16c
ghassen-hammouda <81829678+ghassen-hammouda@users.noreply.github.com>
2022-10-27 00:12:19 +0100

    Update RestaurantController.java

--- src/main/java/tn/esprit/asi/controllers/RestaurantController.java
+++ src/main/java/tn/esprit/asi/controllers/RestaurantController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.List;
 import tn.esprit.asi.entities.Restaurant;
 import tn.esprit.asi.services.IRestaurantService;
 
